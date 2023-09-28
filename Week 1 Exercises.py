7d75bb4d22d4914bdf40b5ccc6ca7c9c6502cde3
Zachary Smith <146333901+ZachFJMS@users.noreply.github.com>
2023-09-28 12:57:13 +0100

    Add files via upload

---
+++ Week 1 Exercises.py
@@ -0,0 +1,21 @@
+print("Hello, World")
+print("Hello, Zach")
+
+Y_Celcius = 38.4
+
+YA_Ferenhigt = (Y_Celcius * 9/5) + 32
+print(YA_Ferenhigt)
+print(Y_Celcius)
+
+batted = 1014
+runs = 48426
+notout = 162
+
+innings = (batted - notout)
+
+print(runs / innings)
+
+group = 24
+Students = 113 + 175 + 12
+print("Full Groups =", Students // 24)
+print("students in final group =", Students % 24)
